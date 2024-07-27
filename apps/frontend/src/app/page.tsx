0e99d664c92ad1d0e559cd50d434937f21903b33
moabdev <macenamoab3@gmail.com>
2024-07-27 00:09:43 -0300

    chore: add initial project files

---
+++ apps/frontend/src/app/page.tsx
@@ -0,0 +1,5 @@
+import { somar } from "@barba/core";
+
+export default function Home() {
+  return <div className="">{somar(2, 3)}</div>;
+}
---
+++ apps/mobile/src/app/App.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text>App</Text>
+    </View>
+  );
+};
